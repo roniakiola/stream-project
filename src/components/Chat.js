68aba4e2650e1bcd9d1bf53e4b6b2a5cbcad47ab
Roni Akiola <roniak@metropolia.fi>
2022-09-12 20:16:58 +0300

    chat functioning properly and some basic styling included

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,6 +2,7 @@ import '../css/index.css';
 import Button from './Button';
 import io from 'socket.io-client';
 import { useEffect, useState } from 'react';
+import MessageContainer from './Container';
 
 const socket = io.connect('http://localhost:3001');
 
@@ -9,6 +10,8 @@ const Chat = () => {
   const [username, setUsername] = useState('');
   const [room, setRoom] = useState('');
   const [message, setMessage] = useState('');
+  const [showChat, setShowChat] = useState(false);
+  const [messageList, setMessageList] = useState([]);
 
   const sendMessage = async () => {
     const date = new Date();
@@ -20,58 +23,80 @@ const Chat = () => {
         timestamp: date.getHours() + ':' + date.getMinutes(),
       };
       await socket.emit('send_message', messageData);
+      setMessageList((list) => [...list, messageData]);
+      setMessage('');
     }
   };
 
   const joinRoom = () => {
     if (username !== '' && room !== '') {
       socket.emit('join_room', room);
+      setShowChat(true);
     }
   };
 
   useEffect(() => {
     socket.on('receive_message', (data) => {
+      setMessageList((list) => [...list, data]);
       console.log(data);
     });
-  });
+  }, [socket]);
 
   return (
-    <>
-      <div className='p-2 flex flex-col items-center justify-between bg-gray-200'>
-        <h3>Join A chat</h3>
-        <input
-          type='text'
-          placeholder='Käyttäjänimi'
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        ></input>
-        <input
-          type='text'
-          placeholder='Huonenumero'
-          onChange={(e) => {
-            setRoom(e.target.value);
-          }}
-        ></input>
-        <Button handleClick={joinRoom} text='Join room' />
-      </div>
-      {/* chat */}
-      <div>
+    <div>
+      {!showChat ? (
+        <div className='p-2 flex flex-col items-center justify-between bg-gray-200'>
+          <h3>Join A chat</h3>
+          <input
+            key='username'
+            type='text'
+            placeholder='Käyttäjänimi'
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <input
+            key='roomId'
+            type='text'
+            placeholder='Huonenumero'
+            onChange={(e) => {
+              setRoom(e.target.value);
+            }}
+            onKeyPress={(e) => {
+              e.key === 'Enter' && joinRoom();
+            }}
+          />
+          <Button handleClick={joinRoom} text='Join room' />
+        </div>
+      ) : (
         <div>
           <p>Live Chat</p>
-        </div>
-        <div>
+          <div>
+            {messageList.map((data) => {
+              return (
+                <MessageContainer
+                  id={username === data.username ? 'you' : 'other'}
+                  data={data}
+                ></MessageContainer>
+              );
+            })}
+          </div>
           <input
+            key='message'
             type='text'
             placeholder='Sano jotain...'
+            value={message}
             onChange={(e) => {
               setMessage(e.target.value);
+            }}
+            onKeyPress={(e) => {
+              e.key === 'Enter' && sendMessage();
             }}
           />
           <Button handleClick={sendMessage} text='Send' />
         </div>
-      </div>
-    </>
+      )}
+    </div>
   );
 };
 
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,7 +1,18 @@
-const Container = () => {
+const MessageContainer = ({ id, data }) => {
   return (
-    <div className='p-2 flex flex-col items-center justify-between bg-gray-200'></div>
+    <div
+      className={`${id === 'you' ? 'bg-burnishedbrown' : 'bg-opal'} p-3 m-1`}
+    >
+      <div>
+        <div className='flex flex-row justify-between'>
+          <p className='font-bold'>{data.username}</p>
+          <p className='text-gray-500'>{data.timestamp}</p>
+        </div>
+        <div className='mt-2 bg-gray-400 rounded p-1 bg-opacity-40'>
+          <p>{data.message}</p>
+        </div>
+      </div>
+    </div>
   );
 };
-
-export default Container;
+export default MessageContainer;
